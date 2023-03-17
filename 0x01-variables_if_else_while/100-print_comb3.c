d1dcba74246e3b300d4f849a741f9a527c0c1864
justme923 <ggg571247@gmail.com>
2023-03-17 15:03:42 -0700

    s

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int i, x;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (x = i + 1; x <= '9'; x++)
+		{
+			putchar(i);
+			putchar(x);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
