2835cc5038e98c26e176dee2874a2213a4d656cc
justme923 <ggg571247@gmail.com>
2023-03-17 08:32:41 -0700

    s

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
