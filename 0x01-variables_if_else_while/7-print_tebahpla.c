8a7f0ece4b6829c1474322a17992cc33a69be68e
justme923 <ggg571247@gmail.com>
2023-03-17 08:25:35 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
