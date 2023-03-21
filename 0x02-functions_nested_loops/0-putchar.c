306ff007c35ab53b76aa8dd51b40d21c43abcd19
justme923 <ggg571247@gmail.com>
2023-03-21 09:36:54 -0700

    s

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
