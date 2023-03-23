51b6ecaa7c5647bed1d46d3bad98fd83b849d93f
justme923 <ggg571247@gmail.com>
2023-03-23 13:08:53 -0700

    s

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,11 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n <= '0')
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i <= n; i++)
+		for (i = '1'; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
