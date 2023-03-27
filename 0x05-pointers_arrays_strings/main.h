c08d2fcb9ba18a944ce3bb053f0d142a7960d9cd
justme923 <ggg571247@gmail.com>
2023-03-27 10:30:20 -0700

    s

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int n);
+void swap_int(inta, int b);
+int _strlen(chars);
+void _puts(char str);
+void print_rev(chars);
+void rev_string(char s);
+void puts2(charstr);
+void puts_half(char str);
+void print_array(inta, int n);
+char _strcpy(chardest, char *src);
+
+#endif
