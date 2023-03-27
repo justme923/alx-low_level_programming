4108bf7829bf7f34049b7a87450535e6167bc9ce
justme923 <ggg571247@gmail.com>
2023-03-27 12:25:11 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev - imprime en reversa
+* @s: string
+* return: 0
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i];
+	_putchar('\n');
+}
